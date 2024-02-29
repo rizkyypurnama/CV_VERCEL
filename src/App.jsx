38afee667bf3da5b7b2bb2ec4fe2bffb46f21234
rizky <rizkypurnama844@gmail.com>
2024-02-29 10:56:44 +0700

    reduce lag

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-gray-950">
-        <StarsCanvas/>
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Home />
